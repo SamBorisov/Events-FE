eba288b698818f5c9020176b5d274b555d5b6b43
SamBorisov <samuil.bo5@gmail.com>
2023-04-20 17:29:35 +0300

    style fixes

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,7 +1,8 @@
 import React from "react"
 import { useState, useEffect } from 'react';
-import { ethers } from 'ethers';
 import { abi } from '../contracts/constants';
+import { ethers, BigNumber } from 'ethers';
+
 
 export default function Event(props) {
 
@@ -9,6 +10,11 @@ export default function Event(props) {
     const [eName, seteName] = useState('');
     const [eDetails, seteDetails] = useState('');
     const [ePrice, setePrice] = useState('');
+    const [endTime,setEndTime] = useState(() => {
+        const storedEndTime = localStorage.getItem('endTime');
+        return storedEndTime === null ? "see end time" : storedEndTime 
+       
+      });
 
     useEffect(() => {
         async function fetchEvent() {
@@ -23,18 +29,30 @@ export default function Event(props) {
           seteName(eventName);
           seteDetails(eventDetails);
           setePrice(eventPrice.toNumber());
+
+          const time = await contract.getEndTimestamp();
+
+            const bigNumber =  BigNumber.from(time);
+            const timestampInSeconds = bigNumber.toNumber();
+
+            const date = new Date(timestampInSeconds * 1000);
+            const dateString = date.toLocaleString(); 
+            setEndTime(dateString)
+            localStorage.setItem('endTime', dateString);
         }
     }
     fetchEvent();
       }, []);
 
 return(
-    <div>
+    <div className="event">
 
-    <h4>{eName}</h4>
+    <h4> {eName}</h4>
     <img src={tokenUri} alt="Token Image" />
-    <h4>{eDetails}</h4>
-    <h4>Price for one ticket: {ePrice} Ether</h4>
+    <h4> {eDetails}</h4>
+    <h4>Price: {ePrice} Ether</h4>
+    <h4>Sale end time : {endTime}</h4>
+
 
     </div>
 
--- src/contracts/constants.js
+++ src/contracts/constants.js
@@ -1,6 +1,11 @@
 import EventContract from "./EventContract.json";
 
+//API keys
 export const APP_INFURA_API_KEY = process.env.INFURA_API_KEY;
 export const EtherscanProvider_API_KEY= process.env.ETHERSCAN_API_KEY;
 
+//contract abi
 export const abi = EventContract.abi;
+
+//smart contract address
+export const eventAddress  = "0xB7A5bd0345EF1Cc5E66bf61BdeC17D2461fBd968";
